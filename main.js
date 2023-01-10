c071bd77c95ee4ffd807439a130f31258b112b91
Maelyn <120160610+maelynluraobs@users.noreply.github.com>
2023-01-10 21:38:02 +0800

    created file

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+/* CONTACT */
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
+
+  /* footer */
+  
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+});
+
+footer_input.addEventListener("blur", () => {
+  if (footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+});
+
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scrolling");
+}
+
